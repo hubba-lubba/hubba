4b2c59f591639b546ed339b7dc95001286f229dd
Eddison So <eddisonso@gmail.com>
2024-02-02 13:00:31 -0500

    Update healthcheck.py

--- user-service/routes/healthcheck.py
+++ user-service/routes/healthcheck.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_cors import CORS
 
-healthcheck = Blueprint('healthcheck', __name__, url_prefix="/")
+healthcheck_blueprint = Blueprint('healthcheck', __name__, url_prefix="/")
 CORS(healthcheck)
 
 @healthcheck.route("/")
