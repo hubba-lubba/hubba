beb1a20f0a1615ffdc97faf180a1247f74dbdfe5
Eddison So <eddisonso@gmail.com>
2024-02-02 13:41:04 -0500

    Delete user-service/routes/healthcheck.py

--- user-service/routes/healthcheck.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify, request
-from flask_cors import CORS
-
-healthcheck_blueprint = Blueprint('healthcheck', __name__, url_prefix="/")
-CORS(healthcheck_blueprint)
-
-@healthcheck_blueprint.route("/")
-def readiness():
-    return "Success"
