c75b4ed36a5f09f9b735167c5cc1f00d93aaf662
Eddison So <eddisonso@gmail.com>
2024-02-02 13:41:32 -0500

    Update api.py

--- user-service/api.py
+++ user-service/api.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from routes.user import user_blueprint
-from routes.healthcheck import healthcheck_blueprint
 from flask_cors import CORS
 
 
 def init_app():
     app = Flask(__name__)
     app.register_blueprint(user_blueprint)
-    app.register_blueprint(healthcheck_blueprint)
     cors = CORS(app)
     return app
 
