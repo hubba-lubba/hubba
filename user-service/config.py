1f5239a4827b049d1e9571eff242d4cde92afe91
Eddison So <eddisonso@gmail.com>
2024-02-02 03:37:35 -0500

    Update config.py

--- user-service/config.py
+++ user-service/config.py
@@ -1,6 +1,6 @@
 import os
 
-DB_HOSTNAME = os.environ.get("DB_HOSTNAME", "hubba_db")
+DB_HOSTNAME = os.environ.get("DB_HOSTNAME", "test")
 DB_USER = os.environ.get("DB_USER", "hubba")
 DB_PASSWORD = os.environ.get("DB_PASSWORD", "hubba")
 DB_PORT = os.environ.get("DB_PORT", "5432")
