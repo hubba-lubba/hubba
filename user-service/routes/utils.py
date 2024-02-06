430c98191ad98a3edde4abedfa8a449c81a62977
eddison <eddisonso@gmail.com>
2024-02-06 15:12:06 -0500

    added uuid checking

--- user-service/routes/utils.py
+++ user-service/routes/utils.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from flask import request, jsonify
+import uuid
 
 
 def require_json_params(params):
@@ -26,6 +27,24 @@ def require_query_params(query_args):
                     return jsonify({
                         "status": "failure", "reason": f"missing argument {i}"
                     })
+            return func(*args, **kwargs)
+        return returned_func
+    return decorator
+
+def ensureUUID(field):
+    def decorator(func):
+        @wraps(func)
+        def returned_func(*args, **kwargs):
+            id = None
+            id = request.args.get(field) if request.args.get(field) is not None else request.get_json().get(field)
+            try:
+                uuid.UUID(id)
+            except ValueError:
+                return jsonify({
+                    "status": "failure", 
+                    "reason": "unable to parse uuid"
+                })
+
             return func(*args, **kwargs)
         return returned_func
     return decorator
