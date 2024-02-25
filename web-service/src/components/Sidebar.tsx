95006aea80dd06379ef051426496d052361d6e5b
kevinthuhstink <kev72eat@gmail.com>
2024-02-25 18:49:32 -0500

    restructured sidebar format and style

--- web-service/src/components/Sidebar.tsx
+++ web-service/src/components/Sidebar.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
 import { ChannelButton, EventButton, OrgButton } from './Buttons';
 import defaultimg from '../images/defaultimg.png';
 import '../styles/Sidebar.scss';
 
-export default function () {
+export default function Sidebar() {
     return (
         <div className="sidebar-container">
             <div className="sidebar-library">
-                <p className="library-title">Library</p>
                 <div className="section-block">
-                    <p className="section-title">Channels</p>
+                    <ChannelButton icon={defaultimg} name="Feed" />
+                    <ChannelButton icon={defaultimg} name="Events" />
+                    <ChannelButton icon={defaultimg} name="Organizations" />
+                    <ChannelButton icon={defaultimg} name="Community" />
+                    <ChannelButton icon={defaultimg} name="Education" />
+                </div>
+                <div className="section-block">
+                    <small className="section-title">DISCOVER</small>
                     <ChannelButton icon={defaultimg} name="channel 1" />
                     <ChannelButton icon={defaultimg} name="channel 2" />
                     <ChannelButton icon={defaultimg} name="channel 3" />
@@ -17,18 +22,27 @@ export default function () {
                     <ChannelButton icon={defaultimg} name="channel 5" />
                 </div>
                 <div className="section-block">
-                    <p className="section-title">Events</p>
+                    <small className="section-title">EVENTS</small>
                     <EventButton name="event 1" />
                     <EventButton name="event 2" />
                     <EventButton name="event 3" />
                     <EventButton name="event 4" />
+                    <small className="expand-section">SHOW MORE</small>
                 </div>
                 <div className="section-block">
-                    <p className="section-title">Orgs</p>
+                    <small className="section-title">UPCOMING EVENTS</small>
+                    <OrgButton name="event 1" />
+                    <OrgButton name="event 2" />
+                    <OrgButton name="event 3" />
+                    <OrgButton name="event 4" />
+                </div>
+                <div className="section-block">
+                    <small className="section-title">MY ORGS</small>
                     <OrgButton name="org 1" />
                     <OrgButton name="org 2" />
                     <OrgButton name="org 3" />
                     <OrgButton name="org 4" />
+                    <ChannelButton icon={defaultimg} name="Join an org" />
                 </div>
             </div>
         </div>
