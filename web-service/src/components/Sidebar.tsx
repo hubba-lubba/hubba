adcad578f487403e52ff49b12968b7d6d972f4e8
kevinthuhstink <kev72eat@gmail.com>
2024-02-25 21:25:51 -0500

    adjust buttons positioning/padding

--- web-service/src/components/Sidebar.tsx
+++ web-service/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { ChannelButton, EventButton, OrgButton } from './Buttons';
+import { MenuButton, ChannelButton, EventButton, OrgButton } from './Buttons';
 import defaultimg from '../images/defaultimg.png';
 import '../styles/Sidebar.scss';
 
@@ -7,11 +7,11 @@ export default function Sidebar() {
         <div className="sidebar-container">
             <div className="sidebar-library">
                 <div className="section-block">
-                    <ChannelButton icon={defaultimg} name="Feed" />
-                    <ChannelButton icon={defaultimg} name="Events" />
-                    <ChannelButton icon={defaultimg} name="Organizations" />
-                    <ChannelButton icon={defaultimg} name="Community" />
-                    <ChannelButton icon={defaultimg} name="Education" />
+                    <MenuButton icon={defaultimg} name="Feed" />
+                    <MenuButton icon={defaultimg} name="Events" />
+                    <MenuButton icon={defaultimg} name="Organizations" />
+                    <MenuButton icon={defaultimg} name="Community" />
+                    <MenuButton icon={defaultimg} name="Education" />
                 </div>
                 <div className="section-block">
                     <small className="section-title">DISCOVER</small>
@@ -20,21 +20,14 @@ export default function Sidebar() {
                     <ChannelButton icon={defaultimg} name="channel 3" />
                     <ChannelButton icon={defaultimg} name="channel 4" />
                     <ChannelButton icon={defaultimg} name="channel 5" />
-                </div>
-                <div className="section-block">
-                    <small className="section-title">EVENTS</small>
-                    <EventButton name="event 1" />
-                    <EventButton name="event 2" />
-                    <EventButton name="event 3" />
-                    <EventButton name="event 4" />
                     <small className="expand-section">SHOW MORE</small>
                 </div>
                 <div className="section-block">
                     <small className="section-title">UPCOMING EVENTS</small>
-                    <OrgButton name="event 1" />
-                    <OrgButton name="event 2" />
-                    <OrgButton name="event 3" />
-                    <OrgButton name="event 4" />
+                    <EventButton name="event 1" />
+                    <EventButton name="event 2" />
+                    <EventButton name="event 3" />
+                    <EventButton name="event 4" />
                 </div>
                 <div className="section-block">
                     <small className="section-title">MY ORGS</small>
@@ -42,7 +35,7 @@ export default function Sidebar() {
                     <OrgButton name="org 2" />
                     <OrgButton name="org 3" />
                     <OrgButton name="org 4" />
-                    <ChannelButton icon={defaultimg} name="Join an org" />
+                    <MenuButton icon={defaultimg} name="Join an org" />
                 </div>
             </div>
         </div>
