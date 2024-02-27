0da499f3e7e2047f18edb20c4ede8b6f342c2950
eddison <eddisonso@gmail.com>
2024-02-26 19:07:06 -0500

    modified port

--- user-service/wsgi.py
+++ user-service/wsgi.py
@@ -1,5 +1,6 @@
 from api import init_app
 from logger import LoggerFactory
+from config import *
 
 logger_factory = LoggerFactory()
 logger = logger_factory.get_logger()
@@ -8,4 +9,4 @@ app = init_app()
 logger.info(app.url_map)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=PORT)
