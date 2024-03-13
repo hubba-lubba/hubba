0bb3659f685f33d8d388dc31f1ab68cd4fd1fb68
Kevin Cheng <kev72eat@gmail.com>
2024-03-13 00:53:12 -0400

    add default search bar functions

--- web-service/src/components/elements/SearchBar.tsx
+++ web-service/src/components/elements/SearchBar.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
 import Autosuggest from 'react-autosuggest';
 
-export default function SearchBar() {
+function getSuggestionValue(suggestion: { name: string }) {
+    return suggestion.name
+}
+
+function renderSuggestion(suggestion: { name: string }) {
+    return <span>{suggestion.name}</span>
+}
+
+function renderSectionTitle(section: { title: string }) {
+    return <small>{section.title}</small>
+}
+
+function getSectionSuggestions(section: { recs: { name: string }[] } ) {
+    return section.recs
+}
+
+export default function SearchBar(props: { value: string, onChange: () => null }) {
     return (
         <Autosuggest
             multiSection={true}
-            suggestions={suggestions}
-            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+            suggestions={sampleSuggestions}
+            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={onSuggestionsClearRequested}
             getSuggestionValue={getSuggestionValue}
             renderSuggestion={renderSuggestion}
             renderSectionTitle={renderSectionTitle}
             getSectionSuggestions={getSectionSuggestions}
-            inputProps={inputProps} />
+            inputProps={{...props}} />
     )
 }
