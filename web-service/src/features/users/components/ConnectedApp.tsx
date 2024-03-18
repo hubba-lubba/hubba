b8d24a12d6e2af210e53fd6cbf0b95fca87cca00
Kevin Cheng <kev72eat@gmail.com>
2024-03-18 18:21:27 -0400

    add settings page structure

---
+++ web-service/src/features/users/components/ConnectedApp.tsx
@@ -0,0 +1,5 @@
+export default function ConnectedApp() {
+    return (
+        <div></div>
+    )
+}
--- web-service/src/features/users/components/SettingsForm.tsx
+++ web-service/src/features/users/components/SettingsForm.tsx
@@ -1,3 +1,30 @@
+import ConnectedApp from './ConnectedApp'
+import ChangePassword from './ChangePassword'
+
 export const SettingsForm = () => {
-    return <>Settings</>;
+    return (
+        <div className="flex flex-col items-start">
+            <h1 className="text-3xl mb-8">Settings</h1>
+            <section className="flex flex-row gap-2 mb-6">
+                <label
+                    htmlFor="notifications"
+                    className="text-xl">
+                    Notifications
+                </label>
+                <input
+                    type="checkbox"
+                    id="notifications"
+                    name="notifications"
+                    checked={true} />
+            </section>
+            <section className="flex flex-row gap-2 mb-6">
+                <h2 className="text-2xl">Connected Apps</h2>
+                <ConnectedApp />
+            </section>
+            <section className="flex flex-row gap-2 mb-4">
+                <h2 className="text-2xl">Change Password</h2>
+                <ChangePassword />
+            </section>
+        </div>
+    )
 };
