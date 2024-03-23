5e7e50579189ef8ce4dd546737d2df3ed3e17e94
iscii <issac.zheng9869@gmail.com>
2024-03-23 14:10:46 -0400

    some margin adjustments

--- web-service/src/features/events/components/EventPage.tsx
+++ web-service/src/features/events/components/EventPage.tsx
@@ -11,9 +11,8 @@ export const EventPage = () => {
     const [error, setError] = useState<string>('');
     const [tags, setTags] = useState<React.ReactElement[]>([]);
 
-
     useEffect(() => {
-        if (!id) return //do smt else abt this (but safer)
+        if (!id) return; //do smt else abt this (but safer)
 
         const fetchEventData = async (id: string) => {
             const eventData = await getEvent(id);
@@ -60,49 +59,53 @@ export const EventPage = () => {
     return (
         <div>
             <div className="lg:grid lg:grid-cols-[400px_1fr] lg:grid-rows-4 lg:gap-4">
-                <main className="mb-4 overflow-hidden lg:col-start-2 lg:row-span-3
-                    lg:px-6 lg:flex lg:flex-col lg:gap-2">
-                    <h1 className="text-4xl font-bold mb-6 lg:m-0">
+                <main
+                    className="mb-4 overflow-hidden lg:col-start-2 lg:row-span-3
+                    lg:flex lg:flex-col lg:gap-2 lg:px-6"
+                >
+                    <h1 className="mb-6 text-4xl font-bold lg:m-0">
                         {event.title || `Event ${event.id}`}
                     </h1>
+                    <div className="my-4 flex flex-row gap-2">{tags}</div>
                     <p className="text-2xl font-bold lg:text-xl">
                         {event.status}
                     </p>
-                    <p className="">
-                        {time}
-                    </p>
+                    <p className="">{time}</p>
                     <p className="">Host: {event.host}</p>
                 </main>
 
                 <div className="lg:row-span-full">
                     <Link to={event.url}>
-                        <img src={event.thumbnail || "/public/image_not_found.jpg"}
-                            className="w-full" />
+                        <img
+                            src={
+                                event.thumbnail || '/public/image_not_found.jpg'
+                            }
+                            className="w-full"
+                        />
                     </Link>
-                    <div className="flex flex-row mx-2 my-4 gap-2">
-                        {tags}
-                    </div>
                 </div>
 
-                <button className="rounded-2xl py-2 px-3 w-full bg-hubba-500 font-bold mt-6
-                    lg:col-start-2 lg:relative lg:bottom-4 lg:w-1/3 lg:left-6 lg:mt-0">
-                    {`ENTER: ${event.entryfee}$`}
+                <button
+                    className="mt-6 w-full rounded-2xl bg-hubba-500 px-3 py-2 font-bold
+                    lg:relative lg:bottom-4 lg:left-6 lg:col-start-2 lg:mt-0 lg:w-1/3"
+                >
+                    {'ENTER'}
                 </button>
             </div>
 
-            <section className="my-12 p-8">
-                <h2 className="text-4xl font-bold mb-4">DETAILS</h2>
+            <section className="my-12">
+                <h2 className="mb-4 text-4xl font-bold">DETAILS</h2>
                 <p className="px-4">{event.description || <></>}</p>
             </section>
 
-            <section className="px-8">
-                <h2 className="text-4xl font-bold mb-4">PRIZES</h2>
+            <section>
+                <h2 className="mb-4 text-4xl font-bold">PRIZES</h2>
                 <div className="grid grid-cols-[180px_1fr] gap-8 px-6">
-                    <p className="font-bold text-3xl inline">First Place:</p>
+                    <p className="inline text-3xl font-bold">First Place:</p>
                     <p className="self-end">{event.prizes?.[0]}</p>
-                    <p className="font-bold text-xl inline">Second Place:</p>
+                    <p className="inline text-xl font-bold">Second Place:</p>
                     {event.prizes?.[1]}
-                    <p className="font-bold text-xl inline">Third Place:</p>
+                    <p className="inline text-xl font-bold">Third Place:</p>
                     {event.prizes?.[2]}
                 </div>
             </section>
