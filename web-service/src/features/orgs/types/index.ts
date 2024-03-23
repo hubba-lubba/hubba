1af4be1409a8e5a0a7f3c83e9ed5e6daf6636fa9
iscii <issac.zheng9869@gmail.com>
2024-03-23 18:31:13 -0400

    org page

--- web-service/src/features/orgs/types/index.ts
+++ web-service/src/features/orgs/types/index.ts
@@ -1,10 +1,12 @@
 import { BaseEntity } from '@/types';
+import { User } from '@/features/users/types';
 
 export type Org = {
     name: string;
+    image: string;
     description: string;
     owner: string;
     moderators: string[];
-    users: string[];
+    users: User[];
     events: string[];
 } & BaseEntity;
