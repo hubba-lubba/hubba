279fa8a7344491c6182e899c27535d8b95e66fe9
eddison <eddisonso@gmail.com>
2024-04-02 15:17:25 -0400

    added org routes

--- organizations-service/routes/organizations.py
+++ organizations-service/routes/organizations.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from domains.repositories.events_repository import EventsRepository
+from domains.repositories.organizations_repository import OrganizationsRepository
 from domains.repositories.repo_exceptions import IdMissingException
 from config import VERSION
 from engine import engine
@@ -27,40 +27,28 @@ def version():
 
 @organizations_blueprint.route("/", methods=["PUT"])
 @ensure_authorized()
-@require_json_params(["title", "description", "owner"])
+@require_json_params(["title", "thumbnail", "description", "owner"])
 @ensure_UUID("owner")
-def add_event():
+def add_organization():
     context = request.get_json()
 
-    title = str(context.get("title")) if context.get("title") else None
-    thumbnail = str(context.get("thumbnail")) if context.get("thumbnail") else None
-    description = str(context.get("description")) if context.get("description") else None
-    url = str(context.get("url")) if context.get("url") else None
-    platform = str(context.get("platform")) if context.get("platform") else None
-    tags = list(map(str, context.get("tags"))) if context.get("tags") else []
-    time_of_event = context.get("time_of_event") if context.get("time_of_event") else None
-    host = str(context.get("host")) if context.get("host") else None
-    entry_fee = str(context.get("entry_fee")) if context.get("entry_fee") else None
+    name = context.get("name") if context.get("name") else None
+    image = context.get("image") if context.get("image") else None
+    description = context.get("description") if context.get("description") else None
     owner = context.get("owner")
 
     with Session(engine) as session:
-        events_repository = EventsRepository(session)
+        organizations_repository = OrganizationsRepository(session)
         try:
-            event = events_repository.add_event(
-                title=title,
-                thumbnail=thumbnail,
+            organization = organizations_repository.add_organization(
+                name=name,
+                image=image,
                 description=description,
-                url=url,
-                platform=platform,
-                tags=tags,
-                time_of_event=time_of_event,
-                host=host,
-                entry_fee=entry_fee,
                 owner=owner
             )
             response = jsonify({
                 "status": "success",
-                "event": event.get_JSON()
+                "organization": organization.get_JSON()
             })
             return response
         except IdMissingException as e:
@@ -72,25 +60,18 @@ def add_event():
             return response
 
 @organizations_blueprint.route("/", methods=["GET"])
-@require_query_params(["event_id"])
-@ensure_UUID("event_id")
-def get_event():
-    event_id = request.args.get("event_id")
-    if not event_id:
-        response = jsonify({
-            "status": "error",
-            "message": "event_id is required"
-        })
-        response.status_code = 400
-        return response
+@require_query_params(["organization_id"])
+@ensure_UUID("organization_id")
+def get_organization():
+    organization_id = request.args.get("organization_id")
 
     with Session(engine) as session:
-        events_repository = EventsRepository(session)
+        organizations_repository = OrganizationsRepository(session)
         try:
-            event = events_repository.get_event(event_id=event_id)
+            organization = organizations_repository.get_organization(organization_id=organization_id)
             response = jsonify({
                 "status": "success",
-                "event": event.get_JSON()
+                "organization": organization.get_JSON()
             })
             return response
         except IdMissingException as e:
@@ -100,27 +81,21 @@ def get_event():
             })
             response.status_code = 404
             return response
+
 @organizations_blueprint.route("/", methods=["DELETE"])
 @ensure_authorized()
-@require_query_params(["event_id"])
-@ensure_UUID("event_id")
+@require_query_params(["organization_id"])
+@ensure_UUID("organization_id")
 def delete_event():
-    event_id = request.args.get("event_id")
-    if not event_id:
-        response = jsonify({
-            "status": "error",
-            "message": "event_id is required"
-        })
-        response.status_code = 400
-        return response
+    organization_id = request.args.get("organization_id")
     
     with Session(engine) as session:
-        events_repository = EventsRepository(session)
+        organizations_repository = OrganizationsRepository(session)
         try:
-            event_id = events_repository.delete_event(event_id=event_id)
+            organization_id = organizations_repository.delete_organization(organization_id=organization_id)
             response = jsonify({
                 "status": "success",
-                "event_id": event_id
+                "organization_id": organization_id 
             })
             return response
         except IdMissingException as e:
