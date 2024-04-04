0ec0291b13b5bba6b3012d1a1259ff497c1687a5
eddison <eddisonso@gmail.com>
2024-04-04 15:26:49 -0400

    used env var for service account

--- file-service/config.py
+++ file-service/config.py
@@ -1,1 +1,4 @@
+import os 
+
 PORT = 8010
+SERVICE_ACCOUNT = os.environ.get("SERVICE_ACCOUNT", None)
--- file-service/routes.py
+++ file-service/routes.py
@@ -5,9 +5,21 @@ import datetime
 from authorization import ensure_authorized
 import random
 import string
+from config import SERVICE_ACCOUNT
+import json
+from logger import LoggerFactory
 
+logger_factory = LoggerFactory()
+logger = logger_factory.get_logger()
 blob_url_generator = Blueprint("blob_url_generator", __name__, url_prefix="/")
-credentials = service_account.Credentials.from_service_account_file("./hubba-credentials.json")
+
+if SERVICE_ACCOUNT is None or not SERVICE_ACCOUNT:
+    credentials = service_account.Credentials.from_service_account_file("./hubba-credentials.json")
+else:
+    logger.info("Using service account from environment variable")
+    SA_json = json.loads(SERVICE_ACCOUNT)
+    credentials = service_account.Credentials.from_service_account_info(SA_json)
+
 storage_client = storage.Client(credentials=credentials)
 
 @blob_url_generator.route("/", methods=["GET"])
@@ -20,7 +32,8 @@ def get_profile_picture_upload_url():
     bucket_name = "hubba-profile-pictures"
 
     bucket = storage_client.bucket(bucket_name)
-    blob_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=32)) + ".jpg"
+    random_32 = ''.join(random.choices(string.ascii_lowercase + string.digits, k=32))
+    blob_name =  f"{random_32}.jpeg"
     blob = bucket.blob(blob_name)
 
     url = blob.generate_signed_url(
@@ -29,6 +42,7 @@ def get_profile_picture_upload_url():
         method="PUT",
         content_type="image/jpeg"
     )
+    logger.info(f"Generated signed URL for {blob_name}")
     return jsonify({
         "status": "success",
         "url": url,
