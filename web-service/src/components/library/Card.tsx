0dcb21febfab374aea5be3eaabef3ced2b88d1d4
Issac <issac.zheng9869@gmail.com>
2024-04-04 14:29:26 -0400

    remvoe comments, prettify

--- web-service/src/components/library/Card.tsx
+++ web-service/src/components/library/Card.tsx
@@ -1,8 +1,3 @@
-// user livestreams onlyneed channel; but we want to include user pfp, name
-// events may or may not be livestreams, show iframe if no thumbnail. if thumbnail, link to event page.
-// orgs only have thumbnail (image), name, and description.
-// videos only need video thumbnail and title https://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
-
 import { useNavigate } from 'react-router-dom';
 
 export type CardProps = {
@@ -16,11 +11,11 @@ export const Card = ({ children, url, internal = true }: CardProps) => {
     return (
         <div
             className="flex w-full cursor-pointer flex-col"
-            onClick={() => (internal ? navigate(url) : window.open(url, '_blank'))}
+            onClick={() =>
+                internal ? navigate(url) : window.open(url, '_blank')
+            }
         >
             {children}
         </div>
     );
 };
-// TODO: YOU ARE HERE. set up card header (bottom) and create cards for event, org, and video.
-// TODO: then, update cards across pages
