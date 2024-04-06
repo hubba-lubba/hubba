56732b7ce0072e0e8ed19c126255079b678e6aa7
Britney Yang <byang.careers@gmail.com>
2024-04-06 15:03:56 -0400

    make mockdata for event frontend and fixed event backend mockdata

--- events-service/engine.py
+++ events-service/engine.py
@@ -27,36 +27,156 @@ if RESET_DB:
     with Session(engine) as session:
 
         #TODO: Write a script to populate the database with some users
-        event1 = Events("Esports Tournament: League of Legends", "https://example.com/league_of_legends_thumbnail.jpg", "Compete in our annual League of Legends tournament and prove your skills!", "https://example.com/league_of_legends_tournament", ["gaming", "esports", "League of Legends"], "2025-03-31 08:45:00 UTC", "upcoming", "Gamer's Arena", ["Cash Prize", "Exclusive In-Game Items"])
 
-        event2 = Events("Online Gaming Marathon", "https://example.com/gaming_marathon_thumbnail.jpg", "Join us for a 24-hour gaming marathon where we'll play various online games together!", "https://example.com/gaming_marathon", ["gaming", "marathon", "online gaming"], "2025-03-31 08:45:00 UTC", "upcoming", "Gaming Society", ["Streaming Equipment", "Gaming Merchandise"])
+        event1 = Events(title="Esports Tournament: League of Legends", 
+                        thumbnail="https://example.com/league_of_legends_thumbnail.jpg", 
+                        description="Compete in our annual League of Legends tournament and prove your skills!", 
+                        url="https://example.com/league_of_legends_tournament", 
+                        tags=["gaming", "esports", "League of Legends"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Gamer's Arena", 
+                        prizes=["Cash Prize", "Exclusive In-Game Items"])
 
-        event3 = Events("Call of Duty: Warzone Tournament", "https://example.com/call_of_duty_thumbnail.jpg", "Test your skills in our Call of Duty: Warzone tournament and compete for glory!", "https://example.com/warzone_tournament", ["gaming", "esports", "Call of Duty"], "2025-03-31 08:45:00 UTC", "upcoming", "Warzone Warriors", ["Cash Prize", "Exclusive In-Game Items"])
+        event2 = Events(title="Online Gaming Marathon", 
+                        thumbnail="https://example.com/gaming_marathon_thumbnail.jpg", 
+                        description="Join us for a 24-hour gaming marathon where we'll play various online games together!", 
+                        url="https://example.com/gaming_marathon", 
+                        tags=["gaming", "marathon", "online gaming"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Gaming Society", 
+                        prizes=["Streaming Equipment", "Gaming Merchandise"])
 
-        event4 = Events("Fortnite Battle Royale Event", "https://example.com/fortnite_thumbnail.jpg", "Join us for an epic Fortnite Battle Royale event where we'll battle it out for victory royale!", "https://example.com/fortnite_event", ["gaming", "Fortnite", "Battle Royale"], "2025-03-31 08:45:00 UTC", "upcoming", "Fortnite Fanatics", ["V-Bucks", "Cosmetic Items"])
+        event3 = Events(title="Call of Duty: Warzone Tournament", 
+                        thumbnail="https://example.com/call_of_duty_thumbnail.jpg", 
+                        description="Test your skills in our Call of Duty: Warzone tournament and compete for glory!", 
+                        url="https://example.com/warzone_tournament", 
+                        tags=["gaming", "esports", "Call of Duty"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Warzone Warriors", 
+                        prizes=["Cash Prize", "Exclusive In-Game Items"])
 
-        event5 = Events("FIFA World Cup: Virtual Edition", "https://example.com/fifa_virtual_thumbnail.jpg", "Experience the thrill of the FIFA World Cup in our virtual edition tournament!", "https://example.com/fifa_virtual_cup", ["gaming", "esports", "FIFA"], "2025-03-31 08:45:00 UTC", "upcoming", "Virtual FIFA Federation", ["Cash Prize", "Exclusive In-Game Items"])
+        event4 = Events(title="Fortnite Battle Royale Event", 
+                        thumbnail="https://example.com/fortnite_thumbnail.jpg", 
+                        description="Join us for an epic Fortnite Battle Royale event where we'll battle it out for victory royale!", 
+                        url="https://example.com/fortnite_event", 
+                        tags=["gaming", "Fortnite", "Battle Royale"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Fortnite Fanatics", 
+                        prizes=["V-Bucks", "Cosmetic Items"])
 
-        event6 = Events("Overwatch League Championship", "https://example.com/overwatch_championship_thumbnail.jpg", "Witness the best teams compete in the Overwatch League Championship for ultimate glory!", "https://example.com/overwatch_championship", ["gaming", "esports", "Overwatch"], "2025-03-31 08:45:00 UTC", "upcoming", "Overwatch League", ["Championship Trophy", "Exclusive Skins"])
+        event5 = Events(title="FIFA World Cup: Virtual Edition", 
+                        thumbnail="https://example.com/fifa_virtual_thumbnail.jpg", 
+                        description="Experience the thrill of the FIFA World Cup in our virtual edition tournament!", 
+                        url="https://example.com/fifa_virtual_cup", 
+                        tags=["gaming", "esports", "FIFA"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Virtual FIFA Federation", 
+                        prizes=["Cash Prize", "Exclusive In-Game Items"])
 
-        event7 = Events("Minecraft Building Competition", "https://example.com/minecraft_building_thumbnail.jpg", "Show off your creativity in our Minecraft Building Competition and win amazing prizes!", "https://example.com/minecraft_competition", ["gaming", "Minecraft", "Building"], "2025-03-31 08:45:00 UTC", "upcoming", "BlockCrafters Community", ["Gift Cards", "Exclusive Blocks"])
+        event6 = Events(title="Overwatch League Championship", 
+                        thumbnail="https://example.com/overwatch_championship_thumbnail.jpg", 
+                        description="Witness the best teams compete in the Overwatch League Championship for ultimate glory!", 
+                        url="https://example.com/overwatch_championship", 
+                        tags=["gaming", "esports", "Overwatch"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Overwatch League", 
+                        prizes=["Championship Trophy", "Exclusive Skins"])
 
-        event8 = Events("Super Smash Bros. Ultimate Tournament", "https://example.com/smash_bros_thumbnail.jpg", "Compete in our Super Smash Bros. Ultimate tournament and emerge as the champion!", "https://example.com/smash_bros_tournament", ["gaming", "Super Smash Bros.", "Fighting"], "2025-03-31 08:45:00 UTC", "upcoming", "Smash Masters Association", ["Cash Prize", "Exclusive Characters"])
+        event7 = Events(title="Minecraft Building Competition", 
+                        thumbnail="https://example.com/minecraft_building_thumbnail.jpg", 
+                        description="Show off your creativity in our Minecraft Building Competition and win amazing prizes!", 
+                        url="https://example.com/minecraft_competition", 
+                        tags=["gaming", "Minecraft", "Building"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="BlockCrafters Community", 
+                        prizes=["Gift Cards", "Exclusive Blocks"])
 
-        event9 = Events("World of Warcraft Raid Night", "https://example.com/wow_raid_thumbnail.jpg", "Gather your party and embark on an epic raid night adventure in World of Warcraft!", "https://example.com/wow_raid_night", ["gaming", "World of Warcraft", "MMORPG"], "2025-03-31 08:45:00 UTC", "upcoming", "Azeroth Raiders Guild", ["Legendary Loot", "Mounts"])
+        event8 = Events(title="Super Smash Bros. Ultimate Tournament", 
+                        thumbnail="https://example.com/smash_bros_thumbnail.jpg", 
+                        description="Compete in our Super Smash Bros. Ultimate tournament and emerge as the champion!", 
+                        url="https://example.com/smash_bros_tournament", 
+                        tags=["gaming", "Super Smash Bros.", "Fighting"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Smash Masters Association", 
+                        prizes=["Cash Prize", "Exclusive Characters"])
 
-        event10 = Events("Rocket League Championship", "https://example.com/rocket_league_championship_thumbnail.jpg", "Compete in our Rocket League Championship and score amazing goals to claim victory!", "https://example.com/rocket_league_championship", ["gaming", "Rocket League", "Sports"], "2025-03-31 08:45:00 UTC", "upcoming", "Rocket League Federation", ["Cash Prize", "Exclusive Car Designs"])
+        event9 = Events(title="World of Warcraft Raid Night", 
+                        thumbnail="https://example.com/wow_raid_thumbnail.jpg", 
+                        description="Gather your party and embark on an epic raid night adventure in World of Warcraft!", 
+                        url="https://example.com/wow_raid_night", 
+                        tags=["gaming", "World of Warcraft", "MMORPG"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Azeroth Raiders Guild", 
+                        prizes=["Legendary Loot", "Mounts"])
 
-        event11 = Events("Dota 2 International Tournament", "https://example.com/dota2_international_thumbnail.jpg", "Prepare for intense battles in the Dota 2 International Tournament and compete for the Aegis of Champions!", "https://example.com/dota2_international", ["gaming", "esports", "Dota 2"], "2025-03-31 08:45:00 UTC", "upcoming", "Valve Corporation", ["Aegis of Champions", "Cash Prize"])
+        event10 = Events(title="Rocket League Championship", 
+                        thumbnail="https://example.com/rocket_league_championship_thumbnail.jpg", 
+                        description="Compete in our Rocket League Championship and score amazing goals to claim victory!", 
+                        url="https://example.com/rocket_league_championship", 
+                        tags=["gaming", "Rocket League", "Sports"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Rocket League Federation", 
+                        prizes=["Cash Prize", "Exclusive Car Designs"])
 
-        event12 = Events("Apex Legends Battle Royale Event", "https://example.com/apex_legends_thumbnail.jpg", "Join us for an adrenaline-fueled Apex Legends Battle Royale event where only the best squad survives!", "https://example.com/apex_legends_event", ["gaming", "Apex Legends", "Battle Royale"], "2025-03-31 08:45:00 UTC", "upcoming", "Legends Arena", ["Apex Coins", "Exclusive Skins"])
-        
-        event13 = Events("Hearthstone Championship", "https://example.com/hearthstone_championship_thumbnail.jpg", "Compete in our Hearthstone Championship and showcase your strategic card-playing skills!", "https://example.com/hearthstone_championship", ["gaming", "Hearthstone", "Card Game"], "2025-03-31 08:45:00 UTC", "upcoming", "Hearthstone Masters League", ["Championship Trophy", "Card Packs"])
+        event11 = Events(title="Dota 2 International Tournament", 
+                        thumbnail="https://example.com/dota2_international_thumbnail.jpg", 
+                        description="Prepare for intense battles in the Dota 2 International Tournament and compete for the Aegis of Champions!", 
+                        url="https://example.com/dota2_international", 
+                        tags=["gaming", "esports", "Dota 2"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Valve Corporation", 
+                        prizes=["Aegis of Champions", "Cash Prize"])
 
-        event14 = Events("Pokémon GO Community Day", "https://example.com/pokemon_go_community_day_thumbnail.jpg", "Join us for a Pokémon GO Community Day where we'll catch rare Pokémon and complete challenges together!", "https://example.com/pokemon_go_community_day", ["gaming", "Pokémon GO", "Mobile"], '2025-03-31 08:45:00 UTC', "upcoming", "Pokémon Trainers Club", ["Exclusive Pokémon Encounters", "Incubators"])
+        event12 = Events(title="Apex Legends Battle Royale Event", 
+                        thumbnail="https://example.com/apex_legends_thumbnail.jpg", 
+                        description="Join us for an adrenaline-fueled Apex Legends Battle Royale event where only the best squad survives!", 
+                        url="https://example.com/apex_legends_event", 
+                        tags=["gaming", "Apex Legends", "Battle Royale"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Legends Arena", 
+                        prizes=["Apex Coins", "Exclusive Skins"])
 
-        event15 = Events("Virtual Reality Gaming Expo", "https://example.com/vr_gaming_expo_thumbnail.jpg", "Experience the future of gaming in our Virtual Reality Gaming Expo with cutting-edge VR technology!", "https://example.com/vr_gaming_expo", ["gaming", "Virtual Reality", "Expo"], '2025-03-31 08:45:00 UTC', "upcoming", "VR Innovations", ["VR Headsets", "Game Demos"])
-        
+        event13 = Events(title="Hearthstone Championship", 
+                        thumbnail="https://example.com/hearthstone_championship_thumbnail.jpg", 
+                        description="Compete in our Hearthstone Championship and showcase your strategic card-playing skills!", 
+                        url="https://example.com/hearthstone_championship", 
+                        tags=["gaming", "Hearthstone", "Card Game"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Hearthstone Masters League", 
+                        prizes=["Championship Trophy", "Card Packs"])
+
+        event14 = Events(title="Pokémon GO Community Day", 
+                        thumbnail="https://example.com/pokemon_go_community_day_thumbnail.jpg", 
+                        description="Join us for a Pokémon GO Community Day where we'll catch rare Pokémon and complete challenges together!", 
+                        url="https://example.com/pokemon_go_community_day", 
+                        tags=["gaming", "Pokémon GO", "Mobile"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="Pokémon Trainers Club", 
+                        prizes=["Exclusive Pokémon Encounters", "Incubators"])
+
+        event15 = Events(title="Virtual Reality Gaming Expo", 
+                        thumbnail="https://example.com/vr_gaming_expo_thumbnail.jpg", 
+                        description="Experience the future of gaming in our Virtual Reality Gaming Expo with cutting-edge VR technology!", 
+                        url="https://example.com/vr_gaming_expo", 
+                        tags=["gaming", "Virtual Reality", "Expo"], 
+                        time_of_event="2025-03-31 08:45:00 UTC", 
+                        status="upcoming", 
+                        platform="VR Innovations", 
+                        prizes=["VR Headsets", "Game Demos"])
         # Add event1 and publish action for event1
         session.add(event1)
         publisher.publish(action=True, uuid=event1.event_id)
