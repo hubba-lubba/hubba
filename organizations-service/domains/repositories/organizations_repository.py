134ae470a77262994b3b1f8cc7b2a5928991d45a
eddison <eddisonso@gmail.com>
2024-04-15 18:59:27 -0400

    added organization routes

--- organizations-service/domains/repositories/organizations_repository.py
+++ organizations-service/domains/repositories/organizations_repository.py
@@ -76,6 +76,17 @@ class OrganizationsRepository:
         return organization_id
 
     """
+    Updates organization object to be persisted using organization object
+
+    :param organization: Organization object to be updated
+    :return: Organizations of updated organization
+    """
+    def _update_organization(self, organization):
+        self.session.patch(organization)
+        self.session.commit()
+        return organization
+
+    """
     Updates Organizations object to be persisted using organization parameters
 
     :param organization_id: uuid of organization_id
@@ -93,6 +104,24 @@ class OrganizationsRepository:
         organization.image = image if image else organization.image
         organization.description = description if description else organization.description
 
-        self.session.patch(organization)
-        self.session.commit()
-        return organization
+        return self._update_organization(organization)
+
+    @check_id_exists(Organizations, ["organization_id"])
+    @check_id_exists(User, ["user_id"])
+    def add_user(self, organization_id=None, user_id=None):
+        organization = self.get_organization(organization_id)
+        user = self.session.get(User, user_id)
+        if not organization: return
+
+        organization.users = list(set(organization.users + [user]))
+        return self._update_organization(organization)
+
+    @check_id_exists(Organizations, ["organization_id"])
+    @check_id_exists(User, ["user_id"])
+    def delete_user(self, organization_id=None, user_id=None):
+        organization = self.get_organization(organization_id=organization_id)
+        user = self.session.get(User, user_id)
+        if not organization: return
+
+        organization.users = list(set(organization.users) - set([user]))
+        return self._update_organization(organization)
