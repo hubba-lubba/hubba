8a07434f0828372e00e48264c94c83ef3e1de8e9
Issac <issac.zheng9869@gmail.com>
2024-04-25 14:57:17 -0400

    object-fit cover for pfp to prevent height mismatch

--- web-service/src/components/elements/Pfp.tsx
+++ web-service/src/components/elements/Pfp.tsx
@@ -1,6 +1,7 @@
 import { FaRegUserCircle } from 'react-icons/fa';
 import { AiOutlineUsergroupAdd } from 'react-icons/ai';
 import { MdPlaylistAdd } from 'react-icons/md';
+import clsx from 'clsx';
 interface PfpProps {
     image?: string;
     variant?: 'user' | 'org' | 'event';
@@ -13,7 +14,10 @@ export const Pfp = ({ image, variant = 'user', size = 40 }: PfpProps) => {
             src={image}
             alt="image"
             width={size}
-            className={variant === 'user' ? 'rounded-full' : ''}
+            className={clsx(
+                'object-cover',
+                variant === 'user' ? 'rounded-full' : '',
+            )}
         />
     ) : variant === 'org' ? (
         <AiOutlineUsergroupAdd size={size} />
